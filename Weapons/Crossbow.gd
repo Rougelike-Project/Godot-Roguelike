b70e95b79c752f1aaba2ac0d72f018f44cc0352c
Mephris <101994151+Mephris@users.noreply.github.com>
2022-05-09 12:46:15 +0200

    Main Game/1st level
    Main Files

---
+++ Weapons/Crossbow.gd
@@ -0,0 +1,19 @@
+extends Weapon
+
+const ARROW_SCENE: PackedScene = preload("res://Weapons/Arrow.tscn")
+
+
+func shoot(offset: int) -> void:
+	var arrow: Area2D = ARROW_SCENE.instance()
+	get_tree().current_scene.add_child(arrow)
+	arrow.launch(global_position, Vector2.LEFT.rotated(deg2rad(rotation_degrees + offset)), 400)
+	
+	
+func triple_shoot() -> void:
+	shoot(0)
+	shoot(3)
+	shoot(-3)
+	shoot(6)
+	shoot(-6)
+	shoot(12)
+	shoot(-12)
